511ec7d252516c2a47af706dbb1dfa6a2b2ae667
GianJeconiah <gianjecositompul06@gmail.com>
2026-01-15 11:41:23 +0700

    Update feature2.cpp

--- feature2.cpp
+++ feature2.cpp
@@ -1,8 +1,27 @@
+#include <iostream>
+using namespace std; 
 
+int main(
 void main(){
   int a = 0;
   if (a>0) cout << "yes" << endl;
 
   #drool
+  return 0;
+}
+
+int a = 10; int b = 20;
+
+void func(){
+  a += b;
+}
+);{
+  int a = 0;
+  if (a>0) cout << "yes" << endl;
+
+  #ifdef DEBUG
+  cout << "Debug mode is on" << endl;
+  #endif
+
   return 0;
 }
